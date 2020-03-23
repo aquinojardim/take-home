8e20df74c3ffd94bc581d89835d646f65209cf50
Josh Godinez <josh.jsx@gmail.com>
2020-03-23 16:03:01 -0700

    created Checkerboard component

---
+++ budee-checkers/src/containers/CheckerBoard/CheckerBoard.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class CheckerBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+          CheckerBoard
+      </div>
+    );
+  }
+}
+
+export default CheckerBoard;
