3dbecaaa5d54cba11903ea4e2b4b25d966cbbce1
Josh Godinez <josh.jsx@gmail.com>
2020-03-23 15:52:05 -0700

    added min and max properties to input and change handler

--- budee-checkers/src/components/Input/index.js
+++ budee-checkers/src/components/Input/index.js
@@ -3,11 +3,30 @@ import React, { Component } from "react";
 class Input extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      chosenValue: 5
+    };
   }
 
+  handleChange = chosenValue => {
+    this.setState({ chosenValue });
+  };
+
+
   render() {
-    return <input type="number" />;
+    const { chosenValue } = this.state;
+
+    return (
+      <div>
+        <input
+          type="number"
+          onChange={event => this.handleChange(event.target.value)}
+          max={20}
+          min={5}
+          value={chosenValue}
+        />
+      </div>
+    );
   }
 }
 
