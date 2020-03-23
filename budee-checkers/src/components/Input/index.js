395c39879d70309beed22d15a73ffd782383159d
Josh Godinez <josh.jsx@gmail.com>
2020-03-23 15:26:33 -0700

    input skeleton created

---
+++ budee-checkers/src/components/Input/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>test</div>;
+  }
+}
+
+export default Input;
